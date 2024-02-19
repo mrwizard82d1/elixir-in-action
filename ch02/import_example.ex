3020a7ffcf94e978f3e81a4cbc3307e34261cc4f
Larry Jones <mrwizard82d1@gmail.com>
2024-02-18 19:38:50 -0600

    Module with an alias

--- ch02/import_example.ex
+++ ch02/import_example.ex
@@ -1,8 +1,18 @@
-defmodule MyModule do
+defmodule MyModuleWithImport do
   import IO
 
   def my_function do
     puts "Calling imported function."
+  end
+
+end
+
+defmodule MyModuleWithAlias do
+
+  alias IO, as: MyIO
+
+  def my_function do
+    MyIO.puts("Calling imported function using alias.")
   end
 
 end
